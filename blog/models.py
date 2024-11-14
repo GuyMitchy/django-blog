d53773553bf79388c40f007468f62a51ab2e80d4
Guy Clements <Guy.Clements123@googlemail.com>
2024-11-14 15:43:06 +0000

    Add ordering methods to pst and comment

--- blog/models.py
+++ blog/models.py
@@ -19,6 +19,14 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
     
     
+    
+    class Meta:
+        ordering = ["-created_on"]
+        
+    def __str__(self):
+        return f"Title: {self.title}, Author: {self.author}"
+    
+    
 class Comment(models.Model):
     
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
@@ -26,5 +34,13 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ["created_on"]
+        
+    def __str__(self):
+        return f"Comment:{self.body}, by {self.author}"
+        
+        
     
 
